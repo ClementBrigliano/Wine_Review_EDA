5995ff29394ed25707347dba9c9f677c91f225c1
falfouljihed <falfoul.jihed@hotmail.com>
2023-11-24 00:13:59 +0100

    using dash

---
+++ app.py
@@ -0,0 +1,51 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import ipywidgets as widgets
+from tools import plot_dist_price_quantiles_quality
+
+df = pd.read_csv('data/winemag-data_first150k.csv',)
+
+# only keep the columns we need points and price
+df = df[['points', 'price']]
+
+# rename points to quality
+df = df.rename(columns={'points': 'quality'})
+
+
+
+# Create widgets
+df_widget = widgets.fixed(df)
+nb_quantiles_slider = widgets.IntSlider(min=2, max=20, step=1, value=2)
+
+# Set up Dash app
+app = dash.Dash(__name__)
+
+# Define layout
+app.layout = html.Div([
+    dcc.Graph(id='interactive-plot'),
+    html.Label('Number of Quantiles'),
+    dcc.Slider(
+        id='nb-quantiles-slider',
+        min=2,
+        max=20,
+        step=1,
+        value=2,
+        marks={i: str(i) for i in range(2, 21)}
+    )
+])
+
+# Define callback to update the plot based on slider value
+@app.callback(
+    Output('interactive-plot', 'figure'),
+    [Input('nb-quantiles-slider', 'value')]
+)
+def update_plot(nb_quantiles):
+    return plot_dist_price_quantiles_quality(df, nb_quantiles)
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- tools.py
+++ tools.py
@@ -6,10 +6,13 @@ def quantiles_data(df:pd.DataFrame, nb_quantiles:int = 2) -> pd.DataFrame:
     This function takes a dataframe and return a dataframe with the quantiles of the dataframe
     """
     df['quality_quantile'] = pd.qcut(df['quality'], nb_quantiles, labels=False, duplicates='drop')
-    
-    # replace each quality_quantile by the min of the quality in the quantile
+
+    df['quality_quantile_labels'] = df['quality_quantile'].astype(str)
+
     for i in range(nb_quantiles):
-        df.loc[df['quality_quantile'] == i, 'quality_quantile'] = f"over {df[df['quality_quantile'] == i]['quality'].min()}"
+        quantiles_min = df[df['quality_quantile'] == i]['quality'].min()
+        quantiles_max = df[df['quality_quantile'] == i]['quality'].max()
+        df.loc[df['quality_quantile'] == i, 'quality_quantile_labels'] = f'{quantiles_min}-{quantiles_max}'
     
     return df
 
@@ -22,8 +25,10 @@ def plot_dist_price_quantiles_quality(df,nb_quantiles:int = 2):
     
     
     df = quantiles_data(df, nb_quantiles)
-    prices = df['price']
     
     # color = quality_quantile to have the same color for each quantile of quality and the labels are the labels of the quantiles
-    fig = px.histogram(df, x=prices, color="quality_quantile")
-    fig.show() 
+    return px.histogram(df, x="price", color="quality_quantile_labels", nbins=1000,
+                        log_y=True,  # Set log scale on the y-axis
+                        opacity=0.7,  # Adjust opacity for overlapping distributions
+                        title="Distribution of Prices Across Quality Quantiles",
+                        labels={"price": "Price", "quality_quantile_labels": "Quality Quantile"})
